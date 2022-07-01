492c6dabcfb5efe0203a906d7db9807da19df376
adebayox <dadebayo200@gmail.com>
2022-07-01 22:38:43 +0100

    everything ready

--- app/src/main/java/com/example/iment/MainActivity.java
+++ app/src/main/java/com/example/iment/MainActivity.java
@@ -21,9 +21,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Retrofit retrofit;
-    private RetrofitInterface retrofitInterface;
-    private String BASE_URL = "";
 
     Handler h = new Handler();
 
@@ -32,26 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        retrofitInterface = retrofit.create(RetrofitInterface.class);
-
-        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleLoginDialogue();
-
-            }
-        });
-        findViewById(R.id.signupbtn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleSignupDialogue();
-            }
-        });
 
 
         //splash
@@ -64,86 +41,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         },4000);
-    }
+    }}
 
-    private void handleLoginDialogue() {
-
-        View view = getLayoutInflater().inflate(R.layout.activity_login, null);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setView(view).show();
-
-        Button login = view.findViewById(R.id.btnLogin);
-        EditText emailEdit = view.findViewById(R.id.edUsername);
-        EditText passwordEdit = view.findViewById(R.id.edPassword);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                HashMap<String, String> map = new HashMap<>();
-
-                map.put("email", emailEdit.getText().toString());
-                map.put("password", passwordEdit.getText().toString());
-
-                Call<LoginResponse> call = retrofitInterface.executeLogin(map);
-
-                call.enqueue(new Callback<LoginResponse>() {
-                    @Override
-                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage(),
-                                Toast.LENGTH_LONG).show();
-
-                    }
-                });
-
-            }
-        });
-    }
-
-    private void handleSignupDialogue() {
-        View view = getLayoutInflater().inflate(R.layout.activity_signup, null);
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(view).show();
-
-        Button signup = view.findViewById(R.id.signupbtn);
-        EditText nameEdit = view.findViewById(R.id.nameEdit);
-        EditText emailEdit = view.findViewById(R.id.nameEdit);
-        EditText passwordEdit = view.findViewById(R.id.passwordEdit);
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                HashMap<String, String> map = new HashMap<>();
-
-                map.put("name", nameEdit.getText().toString());
-                map.put("email", emailEdit.getText().toString());
-                map.put("password", passwordEdit.getText().toString());
-
-                Call<Void> call = retrofitInterface.executeSignup(map);
-
-                call.enqueue(new Callback<Void>() {
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                        Toast.makeText(MainActivity.this, t.getMessage(),
-                                Toast.LENGTH_LONG).show();
-
-                    }
-                });
-            }
-        });
-    }
-}
